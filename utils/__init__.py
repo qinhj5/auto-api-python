af77713a6373c6e3f68ff6f6c44eb28643757e35
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2023-12-27 17:02:43 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,17 +2,19 @@
 import os
 import sys
 from utils.logger import logger
+from utils.decorators import singleton
 from utils.common import (get_env,
                           set_env,
                           get_conf,
-                          set_allure_and_console_output,
-                          get_current_datetime,
+                          get_csv_data,
+                          get_json_data,
                           is_json_string,
-                          loads_json_string,
                           get_code_modifier,
-                          get_csv_data)
-from utils.decorators import singleton
-
+                          loads_json_string,
+                          set_assertion_error,
+                          get_current_datetime,
+                          set_allure_and_console_output,
+                          )
 
 # Path setup
 utils_dir = os.path.dirname(__file__)
