b51482f9af0a9269ab8ee4cb7d11431b56a7f3b5
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-01-03 19:28:50 +0800

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -5,22 +5,25 @@ import colorlog
 import multiprocessing
 from logging.handlers import RotatingFileHandler
 
-# setup directory
+
+class ModuleFilter(logging.Filter):
+    def filter(self, record):
+        return not record.pathname.endswith("multiprocessing/util.py")
+
+
+module_filter = ModuleFilter()
+
+# setup log directory
 utils_dir = os.path.dirname(__file__)
 project_dir = os.path.abspath(os.path.join(utils_dir, ".."))
 log_dir = os.path.abspath(os.path.join(project_dir, "log"))
 os.makedirs(log_dir, exist_ok=True)
 
-# setup file
-test_log_path = os.path.abspath(os.path.join(log_dir, "test.log"))
+# setup log path
+log_path = os.path.abspath(os.path.join(log_dir, "test.log"))
 
 # setup formatter
 log_format = "%(asctime)s - PID:%(process)s - TID:%(thread)s - %(filename)s:%(lineno)d - [%(levelname)s] - %(message)s"
-file_formatter = logging.Formatter(log_format)
-
-# build console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
 console_formatter = colorlog.ColoredFormatter(
     f"%(log_color)s{log_format}",
     log_colors={
@@ -30,16 +33,23 @@ console_formatter = colorlog.ColoredFormatter(
         "ERROR": "bold_red"
     }
 )
+file_formatter = logging.Formatter(log_format)
+
+# build console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(console_formatter)
+console_handler.addFilter(module_filter)
 
 # build file handler
-file_handler = RotatingFileHandler(test_log_path,
+file_handler = RotatingFileHandler(log_path,
                                    maxBytes=1024 * 1024 * 10,
                                    backupCount=3,
                                    mode="a",
                                    encoding="utf-8")
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(file_formatter)
+file_handler.addFilter(module_filter)
 
 # setup logger
 logger = multiprocessing.get_logger()
