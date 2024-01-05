dac49095f6a7d7d48a5ee6c9f671fdc7c9988951
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-01-05 17:24:21 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+from typing import Any
 from utils.decorators import singleton
 from utils.logger import logger, file_formatter
 from utils.common import (get_env,
@@ -20,3 +21,34 @@ from utils.common import (get_env,
 utils_dir = os.path.dirname(__file__)
 project_dir = os.path.abspath(os.path.join(utils_dir, ".."))
 sys.path.append(project_dir)
+
+
+# define a cache
+cache = {}
+
+
+def set_cache(key: str, value: Any) -> None:
+    """
+    Set a value in the cache.
+
+    Args:
+        key (str): The key to set.
+        value (Any): The value to set.
+
+    Returns:
+        None
+    """
+    cache[key] = value
+
+
+def get_cache(key: str) -> Any:
+    """
+    Retrieve a value from the cache.
+
+    Args:
+        key (str): The key to retrieve.
+
+    Returns:
+        Any: The value associated with the key, or None if the key is not found.
+    """
+    return cache.get(key, None)
