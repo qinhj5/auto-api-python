e2004130a55a370cc1bff42c7770d8733e028b68
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-01-08 16:17:37 +0800

    Update mysql_connection.py

--- connection/mysql_connection.py
+++ connection/mysql_connection.py
@@ -1,61 +1,28 @@
 # -*- coding: utf-8 -*-
 import pymysql
 import threading
+from utils import get_conf
 from pymysql import cursors
-from utils import get_conf, singleton
 from sshtunnel import SSHTunnelForwarder
 from typing import Tuple, Any, Optional, List, Dict
 
 pymysql.install_as_MySQLdb()
 
 
-def create_mysql_connection(mysql_conf: dict, ssh_conf: dict = None, use_tunnel: bool = True) -> Tuple[Any, Any]:
-    """
-    Create a MySQL connection.
-
-    Args:
-        mysql_conf (dict): MySQL configuration information.
-        ssh_conf (dict): SSH configuration information.
-        use_tunnel (bool): Whether to use an SSH tunnel. Defaults to True.
-
-    Returns:
-        Tuple[Any, Any]: MySQL connection object and SSH tunnel object (if using an SSH tunnel).
-
-    Raises:
-        Exception: Raised when connection creation fails.
-    """
-
-    if use_tunnel:
-        tunnel = SSHTunnelForwarder(
-            ssh_address=(ssh_conf["ssh_host"], ssh_conf["ssh_port"]),
-            ssh_username=ssh_conf["ssh_user"],
-            ssh_pkey=ssh_conf.get("ssh_key"),
-            ssh_password=ssh_conf.get("ssh_password"),
-            remote_bind_address=(mysql_conf["host"], mysql_conf["port"])
-        )
-        tunnel.start()
-
-        connection = pymysql.connect(
-            host="localhost",
-            port=tunnel.local_bind_port,
-            user=mysql_conf["user"],
-            password=mysql_conf["password"],
-            database=mysql_conf["database"]
-        )
-        return connection, tunnel
-    else:
-        connection = pymysql.connect(
-            host=mysql_conf["host"],
-            port=mysql_conf["port"],
-            user=mysql_conf["user"],
-            password=mysql_conf["password"],
-            database=mysql_conf["database"],
-        )
-        return connection, None
-
-
-@singleton
 class MysqlConnection:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Implement singleton mode.
+
+        Returns:
+            None
+        """
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
         """
         Initialize an instance of the MysqlConnection class.
@@ -63,11 +30,56 @@ class MysqlConnection:
         Returns:
             None
         """
-        self.mysql_conf = get_conf(name="mysql")
-        self.ssh_conf = get_conf(name="ssh")
-        self.connection = None
-        self.tunnel = None
+        self._mysql_conf = get_conf(name="mysql")
+        self._ssh_conf = get_conf(name="ssh")
+        self._connection = None
+        self._tunnel = None
         self._lock = threading.Lock()
+
+    @staticmethod
+    def _create_mysql_connection(mysql_conf: dict, ssh_conf: dict = None, use_tunnel: bool = True) -> Tuple[Any, Any]:
+        """
+        Create a MySQL connection.
+
+        Args:
+            mysql_conf (dict): MySQL configuration information.
+            ssh_conf (dict): SSH configuration information.
+            use_tunnel (bool): Whether to use an SSH tunnel. Defaults to True.
+
+        Returns:
+            Tuple[Any, Any]: MySQL connection object and SSH tunnel object (if using an SSH tunnel).
+
+        Raises:
+            Exception: Raised when connection creation fails.
+        """
+
+        if use_tunnel:
+            tunnel = SSHTunnelForwarder(
+                ssh_address=(ssh_conf["ssh_host"], ssh_conf["ssh_port"]),
+                ssh_username=ssh_conf["ssh_user"],
+                ssh_pkey=ssh_conf.get("ssh_key"),
+                ssh_password=ssh_conf.get("ssh_password"),
+                remote_bind_address=(mysql_conf["host"], mysql_conf["port"])
+            )
+            tunnel.start()
+
+            connection = pymysql.connect(
+                host="localhost",
+                port=tunnel.local_bind_port,
+                user=mysql_conf["user"],
+                password=mysql_conf["password"],
+                database=mysql_conf["database"]
+            )
+            return connection, tunnel
+        else:
+            connection = pymysql.connect(
+                host=mysql_conf["host"],
+                port=mysql_conf["port"],
+                user=mysql_conf["user"],
+                password=mysql_conf["password"],
+                database=mysql_conf["database"],
+            )
+            return connection, None
 
     def _execute_sql(self, sql: str) -> Tuple[int, Any]:
         """
@@ -79,9 +91,10 @@ class MysqlConnection:
         Returns:
             Tuple[int, Any]: Number of affected rows and cursor object.
         """
-        if self.connection is None:
-            self.connection, self.tunnel = create_mysql_connection(mysql_conf=self.mysql_conf, ssh_conf=self.ssh_conf)
-        with self.connection.cursor(cursors.DictCursor) as cursor:
+        if self._connection is None:
+            self._connection, self._tunnel = MysqlConnection._create_mysql_connection(mysql_conf=self._mysql_conf,
+                                                                                      ssh_conf=self._ssh_conf)
+        with self._connection.cursor(cursors.DictCursor) as cursor:
             rows = cursor.execute(sql)
             return rows, cursor
 
@@ -97,7 +110,7 @@ class MysqlConnection:
         """
         with self._lock:
             self._execute_sql(sql)
-            self.connection.commit()
+            self._connection.commit()
 
     def fetchone(self, sql: str) -> Optional[Dict]:
         """
@@ -135,7 +148,7 @@ class MysqlConnection:
             None
         """
         with self._lock:
-            if self.connection:
-                self.connection.close()
-            if self.tunnel:
-                self.tunnel.close()
+            if self._connection:
+                self._connection.close()
+            if self._tunnel:
+                self._tunnel.close()
