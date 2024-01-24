5b1940d65831b595bf56c6e95bb7dac4bba58710
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-01-24 16:57:46 +0800

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-from config.conf import Global
-
-
-# setup project directory
-config_dir = os.path.dirname(__file__)
-project_dir = os.path.abspath(os.path.join(config_dir, ".."))
-sys.path.append(project_dir)
