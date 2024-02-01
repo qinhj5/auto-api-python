b3a96f8431c7b65a51e1e5f431e6ad60c08c729c
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-02-01 14:09:51 +0800

    Add files via upload

---
+++ utils/redis_connection.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+import os
+import filelock
+import traceback
+from redis import StrictRedis
+from utils.logger import logger
+from utils.dirs import lock_dir
+from types import TracebackType
+from utils.common import get_conf
+from sshtunnel import SSHTunnelForwarder
+
+
+class RedisConnection:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs) -> None:
+        """
+        Implement singleton mode.
+
+        Returns:
+            None
+        """
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, redis_conf_name: str = "redis", ssh_conf_name: str = "ssh", use_tunnel: bool = True) -> None:
+        """
+        Initialize an instance of the RedisConnection class.
+
+        Args:
+            redis_conf_name (str): The name of the Redis configuration. Defaults to "redis".
+            ssh_conf_name (str): The name of the SSH configuration. Defaults to "ssh".
+            use_tunnel (bool): Whether to use an SSH tunnel. Defaults to True.
+
+        Returns:
+            None
+        """
+        self._lock = filelock.FileLock(os.path.abspath(os.path.join(lock_dir, f"{redis_conf_name}.lock")))
+        self._redis_conf = get_conf(name=redis_conf_name)
+        self._ssh_conf = get_conf(name=ssh_conf_name)
+        self._connection = None
+        self._use_tunnel = use_tunnel
+
+    def __enter__(self) -> 'RedisConnection':
+        """
+        Context manager method for entering the context.
+
+        Returns:
+            RedisConnection: The current instance of the RedisConnection class.
+        """
+        return self
+
+    def __exit__(self, exc_type: type, exc_val: BaseException, exc_tb: TracebackType) -> None:
+        """
+        Context manager method for exiting the context.
+
+        Args:
+            exc_type (type): The type of the exception (if any) that occurred within the context.
+            exc_val (BaseException): The exception object (if any) that occurred within the context.
+            exc_tb (TracebackType): The traceback object (if any) associated with the exception.
+
+        Returns:
+            None
+        """
+        if exc_type:
+            logger.error(f"an exception of type {exc_type} occurred: {exc_val}")
+
+        if exc_tb:
+            logger.error("".join(traceback.format_tb(exc_tb)))
+
+        self.close()
+
+    @staticmethod
+    def _create_redis_connection(redis_conf: dict,
+                                 ssh_conf: dict,
+                                 use_tunnel: bool) -> StrictRedis:
+        """
+        Create a Redis connection.
+
+        Args:
+            redis_conf (dict): Redis configuration information.
+            ssh_conf (dict): SSH configuration information.
+            use_tunnel (bool): Whether to use an SSH tunnel.
+
+        Returns:
+            Connection: Redis connection object.
+        """
+        if use_tunnel:
+            forwarder = SSHTunnelForwarder(
+                ssh_address=(ssh_conf["ssh_host"], ssh_conf["ssh_port"]),
+                ssh_username=ssh_conf["ssh_user"],
+                ssh_pkey=ssh_conf.get("ssh_key"),
+                ssh_password=ssh_conf.get("ssh_password"),
+                remote_bind_address=(redis_conf["host"], redis_conf["port"])
+            )
+            forwarder.start()
+
+            connection = StrictRedis(
+                host="localhost",
+                port=forwarder.local_bind_port,
+                password=redis_conf["password"],
+                db=redis_conf["db"]
+            )
+            return connection
+        else:
+            connection = StrictRedis(
+                host=redis_conf["host"],
+                port=redis_conf["port"],
+                password=redis_conf["password"],
+                db=redis_conf["db"],
+            )
+            return connection
+
+    def _check_connection(self) -> None:
+        """
+        Check whether the connection is valid.
+
+        Returns:
+            None
+        """
+        try:
+            if self._connection is None:
+                self._connection = RedisConnection._create_redis_connection(redis_conf=self._redis_conf,
+                                                                            ssh_conf=self._ssh_conf,
+                                                                            use_tunnel=self._use_tunnel)
+        except Exception as e:
+            logger.error(f"{e}\n{traceback.format_exc()}")
+            self.close()
+
+    def set(self, name: str, value: str) -> None:
+        """
+        Set the value of a specified key.
+
+        Args:
+            name (str): The name of the key.
+            value (str): The value to be set.
+
+        Returns:
+            None
+
+        """
+        self._check_connection()
+        with self._lock:
+            self._connection.set(name, value)
+
+    def get(self, name: str) -> str:
+        """
+        Retrieve the value of a specified key.
+
+        Args:
+            name (str): The name of the key.
+
+        Returns:
+            str: The value of the key.
+        """
+        self._check_connection()
+        return self._connection.get(name)
+
+    def hset(self, name: str, key: str, value: str) -> None:
+        """
+        Set the value of a specified field in a hash.
+
+        Args:
+            name (str): The name of the hash.
+            key (str): The field key.
+            value (str): The value to be set.
+
+        Returns:
+            None
+
+        """
+        self._check_connection()
+        with self._lock:
+            self._connection.hset(name, key, value)
+
+    def hget(self, name: str, key: str) -> str:
+        """
+        Get the value of a specified field in a hash.
+
+        Args:
+            name (str): The name of the hash.
+            key (str): The field key.
+
+        Returns:
+            str: The value of the field.
+
+        """
+        self._check_connection()
+        return self._connection.hget(name, key)
+
+    def close(self) -> None:
+        """
+        Close the Redis connection.
+
+        Returns:
+            None
+        """
+        if self._connection:
+            self._connection.close()
