1e7aa425f61a42d5ec61eec9046096e1c1ee231d
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-02-06 17:48:09 +0800

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,5 +1,5 @@
-# generate allure html report
+# generate allure html report (allure needs to be installed first)
 allure generate ./report/raw -o ./report/html --clean
 
-# open allure html report
+# open allure html report (browser needs to be installed first)
 allure open ./report/html
