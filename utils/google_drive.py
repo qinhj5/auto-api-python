659cda56e78e483613565f6f12147b77d8cdb408
qinhj5 <109026386+qinhj5@users.noreply.github.com>
2024-02-19 13:54:21 +0800

    Update google_drive.py

--- utils/google_drive.py
+++ utils/google_drive.py
@@ -102,6 +102,31 @@ class GoogleDrive:
 
         self._drive_service = build("drive", "v3", credentials=self._credentials)
 
+    def _create_folder(self, folder_name: str, parent_folder_id: str = None) -> str:
+        """
+        Create a new folder in Google Drive.
+
+        Args:
+            folder_name (str): The name of the folder to create.
+            parent_folder_id (str): The ID of the parent folder. Default is None (create in root).
+
+        Returns:
+            str: The ID of the newly created folder, or None if creation failed.
+        """
+        file_metadata = {
+            "name": folder_name,
+            "mimeType": "application/vnd.google-apps.folder",
+            "parents": [parent_folder_id]
+        }
+
+        response = self._drive_service.files().create(body=file_metadata, fields="id").execute()
+        folder_id = response.get("id")
+        if folder_id is not None:
+            logger.info(f"created folder {folder_name}")
+        else:
+            logger.error(f"failed to create folder, file will be uploaded to root, response: {response}")
+        return folder_id
+    
     def _get_folder_id(self, folder_name: str) -> str:
         """
             Get the ID of a folder by its name.
@@ -115,42 +140,48 @@ class GoogleDrive:
         response = self._drive_service.files().list(
             q=f"""name="{folder_name}" and mimeType="application/vnd.google-apps.folder" """).execute()
 
-        folder_id = None
         folders = response.get("files", [])
         if folders:
             folder_id = folders[0].get("id")
         else:
             logger.error(f"folder {folder_name} not found")
+            folder_id = self._create_folder(folder_name=folder_name)
 
         return folder_id
 
-    def upload_file(self, file_path: str, file_name: str = None, folder_name: str = None) -> dict:
+    def upload_file(self, file_path: str, file_name: str = None) -> bool:
         """
         Upload a file to Google Drive.
 
         Args:
             file_path (str): Path to the file to upload.
             file_name (str): Name of the file on Google Drive. Default is None (use raw filename).
-            folder_name (str): Name of the folder in which to upload the file. Default is None (upload to root).
 
         Returns:
-            dict: Metadata of the uploaded file.
+            bool: True if the file was uploaded successfully, False otherwise.
         """
         file_metadata = {}
         if file_name is None:
             file_name = os.path.basename(file_path)
         file_metadata.update({"name": file_name})
 
-        if folder_name:
-            folder_id = self._get_folder_id(folder_name=folder_name)
-            file_metadata.update({"parents": [folder_id]})
+        folder_name = self._google_conf.get("google_drive").get("folder_name")
+        if not folder_name:
+            folder_name = "test"
+
+        folder_id = self._get_folder_id(folder_name=folder_name)
+        file_metadata.update({"parents": [folder_id]})
 
         media = MediaFileUpload(file_path)
         with self._lock:
             response = self._drive_service.files().create(body=file_metadata, media_body=media, fields="id").execute()
 
-        logger.info(f"uploaded {file_path} to drive")
-        return response
+        if response.get("id") is not None:
+            logger.info(f"""uploaded {file_path} to folder {folder_name}, id: {response.get("id")}""")
+            return True
+        else:
+            logger.error(f"failed to upload, response: {response}")
+            return False
 
 
 if __name__ == "__main__":
